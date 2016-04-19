3d547f818544e14895c16ce2dafaa88062d93b77
Jens W. Klein <jk@kleinundpartner.at>
2016-04-19 12:04:20 +0200

    Back to development: 1.3.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-version = '1.3.3'
+version = '1.3.4.dev0'
 short_description = u"Lineage Add-On: Child Site Local Plone Registry"
 long_description = u'\n\n'.join([
     open('README.rst').read(),
